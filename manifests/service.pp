ef7909e82a342714e4ed71010190d0e10e3bee5e
Ruben Bosch <rbosch@hostnet.nl>
2019-06-24 20:35:18 +0200

    Add dropin for 'restart=always'

--- manifests/service.pp
+++ manifests/service.pp
@@ -5,9 +5,17 @@ class transmission::service (
   $service_name   = $transmission::service_name,
 ) {
   if $service_enable {
+    include ::systemd
+
     service { $service_name:
       ensure => $service_enable,
       enable => $service_enable,
+    }
+
+    systemd::dropin_file { "${service_name}.conf":
+      unit    => "${service_name}.service",
+      content => "[Service]\nRestart=always\n",
+      #      notify  => Service[$service_name],
     }
   }
 }
